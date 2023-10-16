718d73fcb6ebfd2f9e327d318a4088c56e2dae5e
Marina <marvolkova237@yandex.ru>
2023-10-16 19:59:58 +0300

    Commit 02

---
+++ main-service/src/main/java/MainServiceApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * File Name: MainServiceApplication.java
+ * Author: Marina Volkova
+ * Date: 2023-10-16,   3:13 PM (UTC+3)
+ * Description:
+ */
+
+public class MainServiceApplication {
+    public static void main(String[] args) {
+    }
+}
